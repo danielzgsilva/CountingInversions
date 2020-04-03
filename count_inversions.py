49ed0ef9a3e5edbb19733769f570edc0d823b352
Daniel Silva <danielzgsilva@knights.ucf.edu>
2020-04-03 14:22:38 -0400

    FInal working version reading from stdin

--- count_inversions.py
+++ count_inversions.py
@@ -1,29 +1,44 @@
-from arguments import Arguments
+import sys
+import re
+from timeit import default_timer as timer
+import psutil
+import os
 
 
 class InversionCounter:
     def __init__(self, arg):
-        print('--> Preprocessing the input!')
         self.array = self.preprocess(arg)
         self.n = len(self.array)
 
-        self.temp = [0] * self.n
+        self.temp = [None] * self.n
 
-    def preprocess(self, arg):
-        arg[0] = arg[0].replace('[', '')
-        arg[-1] = arg[-1].replace(']', '')
+    def preprocess(self, input):
+        # Build input array from stdin
+        array = []
+        for line in input:
+            for ele in line.split():
+                re.sub('\D', '', ele)
+                if type(ele) is str:
+                    ele = int(ele.replace('[', '').replace(']', ''))
 
-        arg = list(map(lambda x: int(x), arg))
-        return arg
+                array.append(ele)
+
+        return array
 
     def run(self):
-        print('--> Counting inversions in a length {} array'.format(self.n))
+        start = timer()
+        inversion_count = self.count_inversions(0, self.n - 1)
 
-        inversion_count = self.count_inversions(self.array, self.temp, 0, self.n - 1)
+        print(inversion_count)
 
-        print('Found {} inversions!'.format(inversion_count))
+        end = timer()
+        print('Execution time: {} seconds'.format(round(end-start, 5)))
 
-    def count_inversions(self, array, temp, l, r):
+        process = psutil.Process(os.getpid())
+        print('Memory usage: {} mb'.format(process.memory_info().rss * 0.000001))
+        return inversion_count
+
+    def count_inversions(self, l, r):
         """
         Counts the number of inversions found in the given array
         """
@@ -38,15 +53,15 @@ class InversionCounter:
         mid = (r + l) // 2
 
         # Recursively split sub-arrays
-        count += self.count_inversions(array, temp, l, mid)
-        count += self.count_inversions(array, temp, mid + 1, r)
+        count += self.count_inversions(l, mid)
+        count += self.count_inversions(mid + 1, r)
 
         # Merge sub-arrays while counting inversions
-        count += self.merge_and_count(array, temp, l, r, mid)
+        count += self.merge_and_count(l, r, mid)
 
         return count
 
-    def merge_and_count(self, array, temp, l, r, mid):
+    def merge_and_count(self, l, r, mid):
         """
         Merges two sorted arrays while counting the number of inversions that
         will occur in the resulting array
@@ -54,15 +69,15 @@ class InversionCounter:
 
         i = l
         j = mid + 1
-        k = l
+        cur = l
 
         count = 0
 
         while i <= mid and j <= r:
             # No inversion found
-            if array[i] < array[j]:
-                # Sort original array and move the left pointer forward
-                temp[k] = array[i]
+            if self.array[i] < self.array[j]:
+                # Place element in correct position in temporary array and move left pointer forward
+                self.temp[cur] = self.array[i]
                 i += 1
 
             # Inversion found
@@ -73,36 +88,29 @@ class InversionCounter:
                 # a number of inversions equal to the number of elements remaining in the left subarray
                 count += (mid - i + 1)
 
-                # Sort original array and move the right pointer forward
-                temp[k] = array[j]
+                # Place element in correct position in temporary array and move right pointer forward
+                self.temp[cur] = self.array[j]
                 j += 1
 
             # Move pointer for sorted array forward
-            k += 1
+            cur += 1
 
         # Copy any remaining elements from either left or right subarrays to the original array
         while i <= mid:
-            temp[k] = array[i]
+            self.temp[cur] = self.array[i]
             i += 1
-            k += 1
+            cur += 1
 
         while j <= r:
-            temp[k] = array[j]
+            self.temp[cur] = self.array[j]
             j += 1
-            k += 1
+            cur += 1
 
-        for i in range(l, r + 1):
-            array[i] = temp[i]
+        self.array[l:r+1] = self.temp[l:r+1]
 
         return count
 
 
 if __name__ == '__main__':
-    arguments = Arguments()
-
-    args = arguments.parse()
-
-    array = args.array
-
-    inversion_counter = InversionCounter(array)
+    inversion_counter = InversionCounter(sys.stdin)
     inversion_counter.run()
